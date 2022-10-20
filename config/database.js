ee547ac65a64e82b74d27def9140fa7c4fcbed0f
Emiliano Rotta <emilianorottaf@gmail.com>
2022-10-20 16:46:22 -0300

    variables de entorno

--- config/database.js
+++ config/database.js
@@ -1,20 +1,19 @@
 import { Sequelize } from "sequelize";
-// esta es una arquitectura para un microservicio
-// create connection 
-//esto es para crear la coneccion el primer desafio es levantar una base de dato y tener la credenciales
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 const db = new Sequelize(
-  "larnu_demo",  //el nombre de la base de datos que creamos la semana pasada
-  "larnu", //usuario
-  "larnuisgold",  {  //paswore
-  host: "localhost", //direccion (si quiero hacer un servicio y mandarlo a la nbe seria por variable de entorno) no es la forma como lo vamos a hacer en el futuro
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, {
+  host: "localhost", 
   dialect: 'postgres', //dialecto
-  logging: false, //por default es true, cada solicitud l back end se va a mostrar todo. es util cuando aprendemos..
-  port: 5432, //es el puerto por defecto, si tenemos otro tenemos que ponerlo, sino dejamos este
+  logging: false,
+  port: 5432, 
   define: {
-    timestamps: false //tiene el uso de dos campos createup y updatecreate, nosotros aca ledesimos que no haga eso
+    timestamps: false
   }
 });
 
 export default db;
 
-//aca estamos creando microservicios, servicios pequeños que hacen una sola funcionalidad (2 endopoint que hace algo y modifica algo, pero si es mas grande esta forma no es muy util..) por eso vamos a ir cambiando esta estructura a lo largo de la semana, si le ponemos muchasa cosas al servicios se hace complejo de leer.. a nivel de carpeta y de archivo.
